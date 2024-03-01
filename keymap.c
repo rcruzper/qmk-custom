ce40291f71047702ed66503e8a17a9992b9fd712
Raúl Cruz <rcruzper@gmail.com>
2024-03-01 22:39:38 +0100

    remove oneshot commented code

--- keymap.c
+++ keymap.c
@@ -1,14 +1,9 @@
 #include QMK_KEYBOARD_H
 
-// #include "oneshot.h"
-// #include "swapper.h"
-
 #define LA_NAV MO(NAV)
 #define LA_SYM MO(SYM)
 #define LA_NUM MO(NUM)
 #define LA_GAM TG(GAM)
-// #define LA_HRK TG(HRK)
-// #define CMD_ENT MT(MOD_LGUI,KC_ENT)
 
 // shortcurts
 #define WS_L A(S(KC_LEFT))  // select word left
@@ -16,29 +11,15 @@
 #define W_L A(KC_LEFT)      // move word left
 #define W_R A(KC_RIGHT)     // move word right
 #define BOOT QK_BOOTLOADER
-// #define OS_CTRL OSM(MOD_LCTL)
-// #define OS_SHFT OSM(MOD_LSFT)
-// #define OS_ALT OSM(MOD_LALT)
-// #define OS_CMD OSM(MOD_LGUI)
 
+// Layers declarations
 enum layers {
    DEF,
-//    HRK,
    GAM,
    NAV,
    SYM,
    NUM
 };
-
-// enum keycodes {
-//    // Custom oneshot mod implementation with no timers.
-//    OS_SHFT = SAFE_RANGE,
-//    OS_CTRL,
-//    OS_ALT,
-//    OS_CMD,
-
-//    SW_WIN, // Switch to next window (cmd-tab)
-// };
 
 // Left-hand home row mods
 #define CTL_A LCTL_T(KC_A)
@@ -53,20 +34,6 @@ enum layers {
 #define CTL_SCLN RCTL_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-//   [DEF] = LAYOUT(
-//   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-//      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-//   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-//      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-//   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-//      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-//   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-//      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LA_HRK,          LA_GAM,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-//   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-//                                     LA_NUM,  LA_NAV,  CMD_ENT,                   KC_SPC,  LA_SYM,  _______
-//                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-//   ),
 
   [DEF] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
@@ -139,49 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-// bool is_oneshot_cancel_key(uint16_t keycode) {
-//     switch (keycode) {
-//         case LA_NAV:
-//             return true;
-//         default:
-//             return false;
-//     }
-// }
-
-// bool is_oneshot_ignored_key(uint16_t keycode) {
-//     switch (keycode) {
-//         case LA_NAV:
-//         case KC_LSFT:
-//         case OS_SHFT:
-//         case OS_CTRL:
-//         case OS_ALT:
-//         case OS_CMD:
-//             return true;
-//         default:
-//             return false;
-//     }
-// }
-
-// bool sw_win_active = false;
-
-// oneshot_state os_shft_state = os_up_unqueued;
-// oneshot_state os_ctrl_state = os_up_unqueued;
-// oneshot_state os_alt_state = os_up_unqueued;
-// oneshot_state os_cmd_state = os_up_unqueued;
-
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//    update_swapper(&sw_win_active, KC_LGUI, KC_TAB, SW_WIN, keycode, record);
-
-//    update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record);
-//    update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record);
-//    update_oneshot(&os_alt_state, KC_LALT, OS_ALT, keycode, record);
-//    update_oneshot(&os_cmd_state, KC_LCMD, OS_CMD, keycode, record);
-
-//    return true;
-// }
-
 // documentation: https://docs.qmk.fm/#/feature_eeprom
-
 void keyboard_post_init_user(void) { // Call the keymap level matrix init.
     // Set default layer
     rgblight_enable_noeeprom();
@@ -191,17 +116,6 @@ void keyboard_post_init_user(void) { // Call the keymap level matrix init.
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-    // case HRK:
-    //     rgb_matrix_mode(RGB_MATRIX_CUSTOM_homerowkeys_effect);
-    //     break;
-    // case SYM:
-    //     rgblight_sethsv_noeeprom(HSV_RED);
-    //     rgblight_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    //     break;
-    // case NUM:
-    //     rgblight_sethsv_noeeprom(HSV_GREEN);
-    //     rgblight_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    //     break;
     case GAM:
         rgb_matrix_mode(RGB_MATRIX_CUSTOM_gamepad_effect);
         break;
--- rules.mk
+++ rules.mk
@@ -1,5 +1,2 @@
 LTO_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
-
-# SRC += oneshot.c
-# SRC += swapper.c
